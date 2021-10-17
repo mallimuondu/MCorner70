b9fc5988943b7c121105c6fc9856fd8fc0c54445
mallimuondu <mallimuondu@gmail.com>
2021-10-17 13:06:08 +0300

    made changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('',views.indexView,name="home"),
-    path('statementmuondu',views.statementView,name="statementmuondu"),
-    path('login',LoginView.as_view(),name="login_url"),
+    path('statementmuondu/',views.statementView,name="statementmuondu"),
+    path('contribution/',views.contributionView,name="contribution"),
+    path('login/',LoginView.as_view(),name="login_url"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,10 +5,12 @@ from django.contrib.auth.decorators import login_required
 def indexView(request):
     return render(request,'index.html')
 
-
 @login_required
 def statementView(request):
     return render(request,'statementmuondu.html')
+
+def contributionView(request):
+    return render(request,'contribution.html')
 
 def loginView(request):
     return render(request,'login/login.html')
