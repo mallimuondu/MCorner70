3db3ad9285a272069a8cea892e3ed5aaa55237ca
mallimuondu <mallimuondu@gmail.com>
2021-12-24 21:14:22 +0300

    I have updated the home page

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth.views import LoginView
-urlpatterns = [
-    path('',views.indexView,name="home"),
-    path('statementmuondu/',views.statementView,name="statementmuondu"),
-    path('contribution/',views.contributionView,name="contribution"),
-    path('login/',LoginView.as_view(),name="login_url"),
-]
